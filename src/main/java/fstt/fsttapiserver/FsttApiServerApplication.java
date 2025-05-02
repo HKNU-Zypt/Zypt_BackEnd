86339e319fc9867743c037987fff57cf5a5d1323
Heee-oh <siai3647@gmail.com>
2025-05-02 14:14:11 +0900

    ?

--- src/main/java/fstt/fsttapiserver/FsttApiServerApplication.java
+++ src/main/java/fstt/fsttapiserver/FsttApiServerApplication.java
@@ -1,8 +1,5 @@
 package fstt.fsttapiserver;
 
-import fstt.fsttapiserver.livekit.LiveKitSource;
-import io.livekit.server.RoomServiceClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
