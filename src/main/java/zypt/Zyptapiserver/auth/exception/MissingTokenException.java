0bd3d3805802d915253f87b8e57ce823adc05188
Heee_oh <siai3647@gmail.com>
2025-05-04 19:31:00 +0900

    패키지명 변경

--- src/main/java/zypt/Zyptapiserver/auth/exception/MissingTokenException.java
+++
@@ -1,7 +0,0 @@
-package zypt.Zyptapiserver.auth.exception;
-
-public class MissingTokenException extends RuntimeException {
-    public MissingTokenException(String message) {
-        super(message);
-    }
-}
---
+++ src/test/java/zypt/zyptapiserver/domain/SocialTypeTest.java
@@ -0,0 +1,47 @@
+package zypt.zyptapiserver.domain;
+
+import zypt.zyptapiserver.auth.service.KakaoService;
+import zypt.zyptapiserver.auth.service.SocialService;
+import zypt.zyptapiserver.auth.service.SocialServiceFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@AutoConfigureMockMvc
+class SocialTypeTest {
+
+    SocialServiceFactory socialServiceFactory = new SocialServiceFactory(socialTypeSocialServiceMap(new KakaoService()));
+    MockMvc mockMvc;
+
+    public Map<SocialType, SocialService> socialTypeSocialServiceMap(KakaoService kakaoService) {
+        Map<SocialType, SocialService> map = new EnumMap<>(SocialType.class);
+        map.put(SocialType.KAKAO, kakaoService);
+        map.put(SocialType.GOOGLE, kakaoService);
+
+        return map;
+    }
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(socialServiceFactory).build();
+    }
+    @Test
+    void socialTypeCheck() throws Exception {
+        ResultActions perform = mockMvc.perform(get("/a")
+                .header("SocialType", "google"));
+
+        SocialService service = socialServiceFactory.getService(perform.andReturn().getRequest());
+
+        Assertions.assertEquals(new KakaoService(), service);
+    }
+
+}
