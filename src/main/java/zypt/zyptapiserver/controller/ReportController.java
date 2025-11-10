7b8aacb446f330a0cc2c4cf80d3e88b0f3735b85
Heee_oh <siai3647@gmail.com>
2025-11-10 16:37:34 +0900

    임시 에러 제보용 api 생성

---
+++ src/main/java/zypt/zyptapiserver/controller/ReportController.java
@@ -0,0 +1,29 @@
+package zypt.zyptapiserver.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zypt.zyptapiserver.domain.dto.ErrorReportDto;
+import zypt.zyptapiserver.repository.ReportRepository;
+
+@RestController
+@Tag(name = "Report API", description = "버그 제보")
+@RequestMapping("/api/report")
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final ReportRepository repository;
+
+    @PostMapping("/error")
+    public ResponseEntity<String> errorReport(@RequestBody ErrorReportDto errorReportDto) {
+        repository.saveReport(errorReportDto);
+
+        return ResponseEntity.ok("제보 완료");
+    }
+
+}
